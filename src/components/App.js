547aa6cf42de7c60e31ec00b77216cab64e23632
tabutcapital <iantabut3@gmail.com>
2024-12-02 12:57:05 +0300

    deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [image, setImage] = useState(null); 
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message); 
+        setIsLoading(false); 
+      })
+      .catch((error) => {
+        console.error("Error fetching the dog image:", error);
+        setIsLoading(false); 
+      });
+  }, []); 
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p> 
+      ) : (
+        <img src={image} alt="A Random Dog" /> 
+      )}
+    </div>
+  );
+};
+
+export default App;
